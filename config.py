5bdb32ac4703938edc0d523d40dbaa020910c9f5
Arezium <100382649+Arezium@users.noreply.github.com>
2022-06-30 15:11:30 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,9 @@
-import os
+# This was originally a repl i made so i changed alot. If you want to visit my repl: https://replit.com/@AreziumTotallyRealOne/VBot#main.py
 
 class config:
-    # Go to the Secrets tab, in "key" write TOKEN and in "value" write your  token, and then click add new secret
-    global token; token = os.getenv("TOKEN")
-    # If to run a server to keep it alive 24/7. (Use UptimeRobot to do so)
-    global run_server; run_server = True
+    global token; token = ""
   
     global prefix; prefix = 'v.'
     global nitro_sniper; nitro_sniper = False
     
-    # Go to the Secrets tab, in "key" write WEB_URL and in "value" write your  webhook url, and then click add new secret. To disable it, just simply write '' or anything that isnt your webhook url
-    global nitro_sniper_url; nitro_sniper_url = os.getenv("WEB_URL")
+    global nitro_sniper_url; nitro_sniper_url = "https://discord.com/"
